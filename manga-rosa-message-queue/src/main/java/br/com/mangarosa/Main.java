b7c3e76d1e5e18519cdfce7ee3cd9bb9114b571e
Guilherme Rocha <luzguilherme.dev@gmail.com>
2024-10-13 19:13:43 -0300

    fix: últimos detalhes

--- manga-rosa-message-queue/src/main/java/br/com/mangarosa/Main.java
+++ manga-rosa-message-queue/src/main/java/br/com/mangarosa/Main.java
@@ -17,54 +17,57 @@ import br.com.mangarosa.messages.interfaces.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        MessageRepository repository = new Repository();
-        MessageBroker broker = new MessageBroker(repository);
+        public static void main(String[] args) {
+                MessageRepository repository = new Repository();
+                MessageBroker broker = new MessageBroker(repository);
 
-        Topic fastDeliveryItems = new FastDeliveryItems(repository);
-        Topic longDistanceItems = new LongDistanceItems(repository);
+                Topic fastDeliveryItems = new FastDeliveryItems(repository);
+                Topic longDistanceItems = new LongDistanceItems(repository);
 
-        Producer foodDeliveryProducer = new MessageProducer("FoodDeliveryProducer", broker, repository,
-                fastDeliveryItems);
-        Producer fastDeliveryProducer = new MessageProducer("FastDeliveryProducer", broker, repository,
-                fastDeliveryItems);
-        Producer pyMarketPlaceProducer = new MessageProducer("PyMarketPlaceProducer", broker, repository,
-                longDistanceItems);
-        Producer physicPersonDeliveryProducer = new MessageProducer("PhysicPersonDeliveryProducer", broker, repository,
-                longDistanceItems);
+                Producer foodDeliveryProducer = new MessageProducer("FoodDeliveryProducer", broker, repository,
+                                fastDeliveryItems);
+                Producer fastDeliveryProducer = new MessageProducer("FastDeliveryProducer", broker, repository,
+                                fastDeliveryItems);
+                Producer pyMarketPlaceProducer = new MessageProducer("PyMarketPlaceProducer", broker, repository,
+                                longDistanceItems);
+                Producer physicPersonDeliveryProducer = new MessageProducer("PhysicPersonDeliveryProducer", broker,
+                                repository,
+                                longDistanceItems);
 
-        foodDeliveryProducer.addTopic(fastDeliveryItems);
-        pyMarketPlaceProducer.addTopic(longDistanceItems);
+                foodDeliveryProducer.addTopic(fastDeliveryItems);
+                pyMarketPlaceProducer.addTopic(longDistanceItems);
 
-        Consumer fastDeliveryItemsConsumer = new MessageConsumer("FastDeliveryItemsConsumer", fastDeliveryItems,
-                repository);
-        Consumer longDistanceItemsConsumer = new MessageConsumer("LongDistanceItemsConsumer", longDistanceItems,
-                repository);
+                Consumer fastDeliveryItemsConsumer = new MessageConsumer("FastDeliveryItemsConsumer", fastDeliveryItems,
+                                repository);
+                Consumer longDistanceItemsConsumer = new MessageConsumer("LongDistanceItemsConsumer", longDistanceItems,
+                                repository);
 
-        broker.subscribe(fastDeliveryItems.name(), fastDeliveryItemsConsumer);
-        broker.subscribe(longDistanceItems.name(), longDistanceItemsConsumer);
+                broker.subscribe(fastDeliveryItems.name(), fastDeliveryItemsConsumer);
+                broker.subscribe(longDistanceItems.name(), longDistanceItemsConsumer);
 
-        foodDeliveryProducer.sendMessage("Menssagem 1");
-        fastDeliveryProducer.sendMessage("Menssagem 3");
-        pyMarketPlaceProducer.sendMessage("Menssagem 3");
-        physicPersonDeliveryProducer.sendMessage("Menssagem 4");
+                foodDeliveryProducer.sendMessage("Menssagem 1");
+                fastDeliveryProducer.sendMessage("Menssagem 3");
+                pyMarketPlaceProducer.sendMessage("Menssagem 3");
+                physicPersonDeliveryProducer.sendMessage("Menssagem 4");
 
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+                ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
-        Runnable tarefa = new Runnable() {
-            public void run() {
-                int i = 0;
-                List<Message> list = repository.getAllNotConsumedMessagesByTopic(fastDeliveryItems.name());
-                System.out.println("Message not consumed: \n");
-                while (i < list.size()) {
-                    System.out.println(
-                            "Message: " + list.get(i).getMessage() + " IsExperied: " + list.get(i).isExperied());
-                    i++;
-                }
+                Runnable tarefa = new Runnable() {
+                        public void run() {
+                                int i = 0;
+                                List<Message> list = repository
+                                                .getAllNotConsumedMessagesByTopic(fastDeliveryItems.name());
+                                System.out.println("Message not consumed: \n");
+                                while (i < list.size()) {
+                                        System.out.println(
+                                                        "Message: " + list.get(i).getMessage() + " IsExperied: "
+                                                                        + list.get(i).isExperied());
+                                        i++;
+                                }
 
-            };
+                        };
 
-        };
-        scheduler.scheduleAtFixedRate(tarefa, 0, 15, TimeUnit.SECONDS);
-    }
+                };
+                scheduler.scheduleAtFixedRate(tarefa, 0, 15, TimeUnit.SECONDS);
+        }
 }
